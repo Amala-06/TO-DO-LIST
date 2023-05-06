c59300e799b2379255a8343f632d9a976f3c8c3b
Amala-06 <129526942+Amala-06@users.noreply.github.com>
2023-05-06 12:59:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let addb = document.getElementById('addtodo');
+let todoc=document.getElementById('todoc');
+let inputfield=document.getElementById('inputfield');
+addb.addEventListener('click',function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText=inputfield.value;
+    todoc.appendChild(paragraph);
+    inputfield.value="";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration='line-through';
+    }
+    )
+    paragraph.addEventListener('dblclick',function(){
+        todoc.removeChild(paragraph);
+    })
+})
